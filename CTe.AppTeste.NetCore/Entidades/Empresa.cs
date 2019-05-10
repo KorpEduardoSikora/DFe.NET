2faaaf7064abe9c29285443a9a228ffd1b7610e1
eduardo.sikora <eduardo.sikora@viasoft.com.br>
2019-05-10 16:50:28 -0300

    Criação do projeto CTe.AppTeste.NetCore para testes das funções do CTe com suporte ao .NetStandard 2.0.

--- CTe.AppTeste.NetCore/Entidades/Empresa.cs
+++ CTe.AppTeste.NetCore/Entidades/Empresa.cs
@@ -30,39 +30,29 @@
 /* http://www.zeusautomacao.com.br/                                             */
 /* Rua Comendador Francisco josé da Cunha, 111 - Itabaiana - SE - 49500-000     */
 /********************************************************************************/
-using System.Web.Services.Protocols;
+using System;
+using DFe.Classes.Entidades;
 
-namespace CTe.Wsdl.DistribuicaoDFe
+namespace CTe.AppTeste.Entidades
 {
-
-    public class cteCabecMsg : SoapHeader
+    [Serializable]
+    public class Empresa
     {
-        private string cUFField;
+        public string Cnpj { get; set; }
+        public string InscricaoEstadual { get; set; }
+        public string Nome { get; set; }
+        public string NomeFantasia { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public long CodigoIbgeMunicipio { get; set; }
+        public string NomeMunicipio { get; set; }
+        public string Cep { get; set; }
+        public Estado SiglaUf { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public string RNTRC { get; set; }
 
-        private string versaoDadosField;
-
-        private System.Xml.XmlAttribute[] anyAttrField;
-
-        /// <remarks/>
-        public string cUF
-        {
-            get { return this.cUFField; }
-            set { this.cUFField = value; }
-        }
-
-        /// <remarks/>
-        public string versaoDados
-        {
-            get { return this.versaoDadosField; }
-            set { this.versaoDadosField = value; }
-        }
-
-        /// <remarks/>
-        [System.Xml.Serialization.XmlAnyAttributeAttribute()]
-        public System.Xml.XmlAttribute[] AnyAttr
-        {
-            get { return this.anyAttrField; }
-            set { this.anyAttrField = value; }
-        }
     }
 }
